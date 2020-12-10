f5e1f8dcaa7601fc3ba131562050e5ceee1e8f7d
anhnbt-it <anhnbt.it@gmail.com>
2020-12-10 09:28:25 +0700

    first commit

---
+++ src/main/java/com/anhnbt/SpringEmailValidationApplication.java
@@ -0,0 +1,13 @@
+package com.anhnbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmailValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEmailValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anhnbt/springemailvalidation/SpringEmailValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhnbt.springemailvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmailValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
